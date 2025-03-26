3b2e742787ddab0a9cc8278131b2dac789d9a10e
Shivam kumar <146973981+Shivam4401@users.noreply.github.com>
2025-03-26 13:56:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 //your code here!
+let list = document.getElementById("infi-list");
+let lastItem = 10
+for(let i=1 ; i<=10 ; i++){
+	let listItem = document.createElement("li");
+	listItem.textContent = `Item${i}`;
+	list.appendChild(listItem);
+}
 
+list.addEventListener("scroll",()=>{
+	if(list.scrollTop + list.clientHeight >= list.scrollHeight){
+		for(let i=1 ; i<=2 ; i++){
+		let listItem = document.createElement("li");
+	    lastItem++;
+		listItem.textContent = `Item${lastItem}`;
+		list.appendChild(listItem);
+}
+	}
+})
